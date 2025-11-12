f5414ed88be4f4b5b86f171beb72b48b106869c7
odenfis <wmoncada@tecsup.edu.pe>
2025-11-12 11:21:11 -0500

    mie12nov_1121

--- lib/screens/alert_screen.dart
+++ lib/screens/alert_screen.dart
@@ -4,16 +4,45 @@ class AlertScreen extends StatelessWidget {
    
   const AlertScreen({super.key});
   
+
+  void displayDialog(BuildContext context){
+    showDialog(
+      barrierDismissible: true, //permitir cierre del showDialog
+      context: context, 
+      builder:(context) => AlertDialog(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadiusGeometry.circular(15)),
+        title: Text('Alerta de Proceso'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text('Esto es el contenido de mi alerta')
+          ],
+        ),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context), 
+            child: Text('Cancelar', style: TextStyle(fontSize: 17),))
+        ],
+      ));
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
          child: ElevatedButton(
-          onPressed: (){}, 
-          child: Text('PROCESAR', style: TextStyle(color: Colors.white, fontSize: 20),)),
+          onPressed: () => displayDialog(context), 
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+            child: Text('PROCESAR', style: TextStyle(color: Colors.white, fontSize: 20),),
+          )),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){},
+        onPressed: (){
+          Navigator.pop(context);
+        },
         child: Icon(Icons.cloud, color: Colors.white,)
         ),
     );
